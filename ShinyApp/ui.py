0046a0d389035841120f6799d595feee27dbfefb
koba96 <filip.westin96@gmail.com>
2023-10-12 21:07:18 +0200

    Added SQL query to get data from database and made the shiny app files - shiny app files not completed.

---
+++ ShinyApp/ui.py
@@ -0,0 +1,2 @@
+from shiny import ui, render, App
+
--- import-gdp-data.py
+++ import-gdp-data.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 ## Start by getting GDP data ##
 ###############################
 response_api = requests.get(
-    "http://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CN?date=1950:2022&format=json&per_page=10000"
+    "http://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CN?date=1950:2022&format=json&per_page=20000"
 )
 
 data_json=response_api.json()
@@ -24,8 +24,10 @@ df_gdp = df_gdp[['countryiso3code', 'date', 'value']].rename(
     }
 )
 
+df_gdp['date'] = pd.to_numeric(df_gdp['date'])
 df_gdp.dropna(inplace=True)
 
 ## send to database
 engine = create_engine(f'postgresql://{"postgres"}:{"password"}@{"localhost"}:{5432}/{"country-db"}')
 df_gdp.to_sql('gdp', engine, if_exists='replace')
+engine.dispose()
